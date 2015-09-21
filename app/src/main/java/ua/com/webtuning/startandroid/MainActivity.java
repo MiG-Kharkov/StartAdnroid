76cc472ee3a5821424eb8da4ac2e7dedee682293
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-21 19:59:49 +0300

    Lesson 19 Add New Style onButtonClick in XML

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(View view) {
+        Toast.makeText(this, "Test button", Toast.LENGTH_SHORT).show();
     }
 }
