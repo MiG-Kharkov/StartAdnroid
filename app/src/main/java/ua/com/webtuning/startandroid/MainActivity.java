c855840654ba2642dea9973f7022ba574d487a3b
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-21 20:14:26 +0300

    Finish lesson 18

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,18 +1,30 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNum1;
+    EditText etNum2;
+
+    TextView tvResult;
+
+    String oper = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+        tvResult = (TextView) findViewById(R.id.tvResult);
     }
 
     @Override
@@ -38,6 +50,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
+
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        // Проверяем поля на пустоту
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+
+        // читаем EditText и заполняем переменные числами
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+        // определяем нажатую кнопку и выполняем соответствующую операцию
+        // в oper пишем операцию, потом будем использовать в выводе
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+        // формируем строку вывода
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
         Toast.makeText(this, "Test button", Toast.LENGTH_SHORT).show();
     }
 }
