c8e426ca5ff4657dd1a8bbff25464b6319bd5f6b
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-21 22:17:30 +0300

    Lesson 20 animation completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,26 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+
+        tv.setOnCreateContextMenuListener(this);
     }
 
     @Override
@@ -33,5 +43,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Animation anim = null;
+
+        switch (item.getItemId()) {
+            case R.id.alpha:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+                break;
+            case R.id.comb:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
+                break;
+            case R.id.rotate:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
+                break;
+            case R.id.scale:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
+                break;
+            case R.id.translate:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
+                break;
+        }
+        if (anim != null) tv.startAnimation(anim);
+        return super.onContextItemSelected(item);
     }
 }
