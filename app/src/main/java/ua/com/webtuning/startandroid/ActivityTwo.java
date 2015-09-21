f6d1f398860045ce5a3d17a9b7669cb6204a2c35
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-21 22:39:35 +0300

    Lesson 21 TwoActivity completed

--- app/src/main/java/ua/com/webtuning/startandroid/ActivityTwo.java
+++ app/src/main/java/ua/com/webtuning/startandroid/ActivityTwo.java
@@ -1,24 +1,22 @@
 package ua.com.webtuning.startandroid;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_activity_two);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_activity_two, menu);
         return true;
     }
 
@@ -35,11 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void startActivityTwo(View view) {
-        Intent intent = new Intent(this, ActivityTwo.class);
-        startActivity(intent);
-
     }
 }
