6e0f5c3fb3487a9ee8cc70957ff1d5d5e84cb36d
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-22 18:53:12 +0300

    Lesson 21 TwoActivity extra add Dialog

--- app/src/main/java/ua/com/webtuning/startandroid/ActivityTwo.java
+++ app/src/main/java/ua/com/webtuning/startandroid/ActivityTwo.java
@@ -1,11 +1,11 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ActivityTwo extends AppCompatActivity {
+public class ActivityTwo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
