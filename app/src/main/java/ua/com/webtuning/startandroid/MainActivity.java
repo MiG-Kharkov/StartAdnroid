b03df7f527dbe36ec54147e4ba27bf261190bb5c
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-23 23:11:11 +0300

    Lesson 40 step1 - addView

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,37 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        View view = layoutInflater.inflate(R.layout.text, null, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+
+        linLayout.addView(view);
+
+        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
+        Log.d(LOG_TAG, "LayoutParams of view is null: " + (layoutParams == null));
+        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
     }
 
     @Override
