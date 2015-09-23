47d00bd51bb5b10200691d88d26e4d71f49ac363
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-23 19:46:41 +0300

    Lesson 34 completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,34 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = getClass().getSimpleName();
+
+    DBHelper dbHelper;
+    EditText etName, etEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+
+        dbHelper = new DBHelper(this);
     }
 
     @Override
@@ -34,4 +52,81 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onBtnClick(View view) {
+
+        ContentValues contentValues = new ContentValues();
+
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                // подготовим данные для вставки в виде пар: наименование столбца - значение
+
+                contentValues.put("name", name);
+                contentValues.put("email", email);
+                // вставляем запись и получаем ее ID
+                long rowID = db.insert("mytable", null, contentValues);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+                break;
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "===Read from mytable===");
+                Log.d(LOG_TAG, "===Rows: ");
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+
+                if (c.moveToFirst()) {
+                    // определяем номера столбцов по имени в выборке
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+                    do {
+                        Log.d(LOG_TAG, "ID = " + c.getInt(idColIndex) +
+                                ", name = " + c.getString(nameColIndex) +
+                                " email = " + c.getString(emailColIndex));
+
+                    } while (c.moveToNext());
+
+                } else
+                    Log.d(LOG_TAG, "0 row");
+                break;
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "===Clear table==");
+
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows = " + clearCount);
+                break;
+        }
+
+        dbHelper.close();
+    }
+
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        private static final int DB_VERTION = 1;
+
+        public DBHelper(Context context) {
+            super(context, "myDB", null, DB_VERTION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "---Create Database---");
+
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "email text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
 }
