74407b2c063a39786eae10f3d1769061e6fa94e2
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-23 17:23:17 +0300

    Lesson 32 completed

--- app/src/main/java/ua/com/webtuning/startandroid/BrowserActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/BrowserActivity.java
@@ -1,32 +1,28 @@
 package ua.com.webtuning.startandroid;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class BrowserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_browser);
 
-        (findViewById(R.id.btnWeb)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://ya.ru")));
-            }
-        });
+        WebView webView = (WebView) findViewById(R.id.webView);
+        Uri data = getIntent().getData();
+        webView.loadUrl(data.toString());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_browser, menu);
         return true;
     }
 
