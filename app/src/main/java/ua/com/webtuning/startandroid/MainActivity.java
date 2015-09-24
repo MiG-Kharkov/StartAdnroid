e5c40ee426599a0a0eb791e4cfd237ce3234eaba
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-24 17:10:47 +0300

    +Lesson 44 OnScrollListener

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -3,10 +3,11 @@ package ua.com.webtuning.startandroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.SparseBooleanArray;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     final String LOG_TAG = "myLogs";
 
     ListView lvMain;
-    String[] names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvMain = (ListView) findViewById(R.id.lvMain);
-        lvMain.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.names, android.R.layout.simple_list_item_multiple_choice);
+                R.array.names, android.R.layout.simple_list_item_1);
         lvMain.setAdapter(adapter);
 
-        names = getResources().getStringArray(R.array.names);
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
+                        + id);
+            }
+        });
+
+        lvMain.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                Log.d(LOG_TAG, "scroll: firstVisibleItem = " + firstVisibleItem
+                        + ", visibleItemCount" + visibleItemCount
+                        + ", totalItemCount" + totalItemCount);
+            }
+        });
 
 
     }
@@ -56,12 +75,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onBtnClick(View view) {
-        SparseBooleanArray sbArray = lvMain.getCheckedItemPositions();
-        for (int i = 0; i < sbArray.size(); i++) {
-            int key = sbArray.keyAt(i);
-            if (sbArray.get(key))
-                Log.d(LOG_TAG, names[key]);
-        }
-    }
 }
