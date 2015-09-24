b07f4a3b7c6d657f7bb2ff7fc7e17e16b0eb98d6
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-24 16:02:42 +0300

    Lesson 42 used simple_list_item_1

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -4,13 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] names = {"Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.lvMain);
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+        listView.setAdapter(adapter);
+
     }
 
     @Override
