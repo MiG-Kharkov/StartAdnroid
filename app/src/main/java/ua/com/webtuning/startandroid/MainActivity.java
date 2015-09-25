4417f203cc3c890b1c48c701d0728116a641d132
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-25 15:55:43 +0300

    Lesson 48 Completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,52 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String ATTRIBUTE_NAME_TEXT = "text";
+    final String ATTRIBUTE_NAME_CHECKED = "checked";
+    final String ATTRIBUTE_NAME_IMAGE = "image";
+
+    ListView lvSimple;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] texts = {"sometext 1", "sometext 2", "sometext 3",
+                "sometext 4", "sometext 5"};
+        boolean[] checked = {true, false, false, true, false};
+        int img = R.mipmap.ic_launcher;
+
+        ArrayList<Map<String, Object>> data = new ArrayList<>(texts.length);
+        Map<String, Object> m;
+        for (int i = 0; i < texts.length; i++) {
+            m = new HashMap<>();
+            m.put(ATTRIBUTE_NAME_TEXT, texts[i]);
+            m.put(ATTRIBUTE_NAME_CHECKED, checked[i]);
+            m.put(ATTRIBUTE_NAME_IMAGE, img);
+            data.add(m);
+        }
+
+
+        String[] from = {ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_CHECKED, ATTRIBUTE_NAME_IMAGE};
+        int[] to = {R.id.tvText, R.id.cdChecked, R.id.ivImg};
+
+        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvSimple.setAdapter(adapter);
     }
 
     @Override
