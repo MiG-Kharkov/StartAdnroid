8fc5c844e1a7d6281ef2281fdb21b68127dcbb3f
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-27 14:39:00 +0300

    Lesson 51 Completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,56 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CM_DELETE_ID = 1;
+    // имена атрибутов для Map
+    final String ATTRIBUTE_NAME_TEXT = "text";
+    final String ATTRIBUTE_NAME_IMAGE = "image";
+
+    ListView lvSimple;
+    SimpleAdapter sAdapter;
+    ArrayList<Map<String, Object>> data;
+    Map<String, Object> m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // упаковываем данные в понятную для адаптера структуру
+        data = new ArrayList<>();
+        for (int i = 1; i < 5; i++) {
+            m = new HashMap<>();
+            m.put(ATTRIBUTE_NAME_TEXT, "sometext " + i);
+            m.put(ATTRIBUTE_NAME_IMAGE, R.mipmap.ic_launcher);
+            data.add(m);
+        }
+
+        // массив имен атрибутов, из которых будут читаться данные
+        String[] from = {ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_IMAGE};
+        // массив ID View-компонентов, в которые будут вставлять данные
+        int[] to = {R.id.tvText, R.id.ivImg};
+
+        sAdapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvSimple.setAdapter(sAdapter);
+        registerForContextMenu(lvSimple);
+
     }
 
     @Override
@@ -33,5 +73,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(View view) {
+        m = new HashMap<>();
+        m.put(ATTRIBUTE_NAME_TEXT, "sometext " + (data.size() + 1));
+        m.put(ATTRIBUTE_NAME_IMAGE, R.mipmap.ic_launcher);
+        data.add(m);
+        sAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CM_DELETE_ID, 0, "Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == CM_DELETE_ID) {
+            AdapterView.AdapterContextMenuInfo acmi =
+                    (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            data.remove(acmi.position);
+            sAdapter.notifyDataSetChanged();
+            return true;
+
+        }
+        return super.onContextItemSelected(item);
     }
 }
