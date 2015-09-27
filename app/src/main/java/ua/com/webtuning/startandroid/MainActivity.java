bc52de7c2a480398dcd2c68eff51aed65dbc06bb
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-27 22:32:39 +0300

    Lesson 58+59 Dialog Time+Date

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,53 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int DIALOG_DATE = 2;
+    int DIALOG_TIME = 1;
+    int myHour = 14;
+    int myMinute = 35;
+    TextView tvTime, tvDate;
+    OnTimeSetListener myCallBack = new OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            tvTime.setText("Time is " + myHour + " hours " + myMinute + " minutes");
+        }
+    };
+    private int myYear = 2015;
+    private int myMonth = 9;
+    private int myDay = 27;
+    OnDateSetListener myCallBackDate = new OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myMonth = year;
+            myMonth = monthOfYear;
+            myDay = dayOfMonth;
+            tvDate.setText("Date is " + myDay + "." + monthOfYear + "." + myYear);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        tvDate = (TextView) findViewById(R.id.tvDate);
     }
 
     @Override
@@ -33,5 +70,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onclick(View view) {
+        if (view.getId() == R.id.tvDate)
+            showDialog(DIALOG_DATE);
+        else if (view.getId() == R.id.tvTime)
+            showDialog(DIALOG_TIME);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
+            return tpd;
+        } else if (id == DIALOG_DATE) {
+            DatePickerDialog dpd = new DatePickerDialog(this, myCallBackDate, myYear, myMonth, myDay);
+            return dpd;
+        }
+
+        return super.onCreateDialog(id);
     }
 }
