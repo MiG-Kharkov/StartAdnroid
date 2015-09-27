d63d206f8db3afe1682abbc75aebc386673d1ffb
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-27 14:13:22 +0300

    Lesson 50 Completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,56 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    // имена атрибутов для Map
+    final String ATTRIBUTE_NAME_TEXT = "text";
+    final String ATTRIBUTE_NAME_PB = "pb";
+    final String ATTRIBUTE_NAME_LL = "ll";
+
+    ListView lvSimple;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int load[] = {41, 48, 22, 35, 30, 67, 51, 88, 12, 88, 22, 88, 52, 76, 23, 9, 12};
+
+        // упаковываем данные в понятную для адаптера структуру
+        ArrayList<Map<String, Object>> data = new ArrayList<>(
+                load.length);
+        Map<String, Object> m;
+        for (int i = 0; i < load.length; i++) {
+            m = new HashMap<String, Object>();
+            m.put(ATTRIBUTE_NAME_TEXT, "Day " + (i + 1) + ". Load: " + load[i] + "%");
+            m.put(ATTRIBUTE_NAME_PB, load[i]);
+            m.put(ATTRIBUTE_NAME_LL, load[i]);
+            data.add(m);
+        }
+        // массив имен атрибутов, из которых будут читаться данные
+        String[] from = {ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_PB, ATTRIBUTE_NAME_LL};
+        int[] to = {R.id.tvLoad, R.id.pbLoad, R.id.llLoad};
+
+        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+
+        sAdapter.setViewBinder(new MyViewBinder());
+
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvSimple.setAdapter(sAdapter);
+
     }
 
     @Override
@@ -33,5 +73,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MyViewBinder implements SimpleAdapter.ViewBinder {
+        int red = getResources().getColor(R.color.Red);
+        int orange = getResources().getColor(R.color.Orange);
+        int green = getResources().getColor(R.color.Green);
+
+        @Override
+        public boolean setViewValue(View view, Object data, String textRepresentation) {
+            int i = 0;
+            switch (view.getId()) {
+                // LinearLayout
+                case R.id.llLoad:
+                    i = ((Integer) data).intValue();
+                    if (i < 40) view.setBackgroundColor(green);
+                    else if (i < 70) view.setBackgroundColor(orange);
+                    else
+                        view.setBackgroundColor(red);
+                    return true;
+                // ProgressBar
+                case R.id.pbLoad:
+                    i = ((Integer) data).intValue();
+                    ((ProgressBar) view).setProgress(i);
+                    return true;
+            }
+            return false;
+        }
     }
 }
