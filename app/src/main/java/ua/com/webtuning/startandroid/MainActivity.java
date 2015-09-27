350734f7d87251ebb3e1aa8e5728cb2cb4bf6176
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-27 17:59:45 +0300

    Lesson 57 Gride without adjust

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<>(this, R.layout.item, R.id.tvText, data);
+        gvMain = (GridView) findViewById(R.id.gvMain);
+        gvMain.setAdapter(adapter);
+        adjustGrideView();
+    }
+
+    private void adjustGrideView() {
+
     }
 
     @Override
