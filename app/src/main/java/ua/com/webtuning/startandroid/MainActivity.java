a5e72ca0c9586172fe4032fc8a91d14a7fa6f1df
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-29 15:53:05 +0300

    Lesson 67 completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,11 +1,20 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +42,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onclick(View view) {
+        switch (view.getId()) {
+            case R.id.btnDefault:
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle(R.string.dialog_title);
+                progressDialog.setMessage("Message");
+                //add button
+                progressDialog.setButton(Dialog.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                progressDialog.show();
+                break;
+            case R.id.btnHoriz:
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle(R.string.dialog_title);
+                progressDialog.setMessage("Message");
+                //Change style
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                //set max
+                progressDialog.setMax(2148);
+                //turn on animation
+                progressDialog.setIndeterminate(true);
+                progressDialog.show();
+                handler = new Handler() {
+                    public void handleMessage(Message msg) {
+                        progressDialog.setIndeterminate(false);
+                        if (progressDialog.getProgress() < progressDialog.getMax()) {
+                            progressDialog.incrementProgressBy(50);
+                            progressDialog.incrementSecondaryProgressBy(75);
+                            handler.sendEmptyMessageDelayed(0, 100);
+                        } else
+                            progressDialog.dismiss();
+
+                    }
+
+                };
+                handler.sendEmptyMessageDelayed(0, 2000);
+                break;
+        }
     }
 }
