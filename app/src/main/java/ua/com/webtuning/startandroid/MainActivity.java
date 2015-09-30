d9ef4016cabab0cb23e7d5efe0dc81bde043ec7e
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-30 18:43:05 +0300

    Lesson 79 completed.

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,74 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String tmp = "";
+
+        try {
+            XmlPullParser xmlPullParser = prepareXpp();
+
+            while (xmlPullParser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                switch (xmlPullParser.getEventType()) {
+                    case XmlPullParser.START_DOCUMENT:
+                        Log.d(LOG_TAG, "START_DOCUMENT");
+                        break;
+                    case XmlPullParser.START_TAG:
+                        Log.d(LOG_TAG, "START_TAG: name = " + xmlPullParser.getName()
+                                + ", depth = " + xmlPullParser.getDepth() + ", attrCount = "
+                                + xmlPullParser.getAttributeCount());
+                        tmp = "";
+                        for (int i = 0; i < xmlPullParser.getAttributeCount(); i++) {
+                            tmp += xmlPullParser.getAttributeName(i) + " = "
+                                    + xmlPullParser.getAttributeValue(i) + ", ";
+                        }
+                        if (!TextUtils.isEmpty(tmp)) Log.d(LOG_TAG, "Attributes: " + tmp);
+                        break;
+                    // конец тэга
+                    case XmlPullParser.END_TAG:
+                        Log.d(LOG_TAG, "END_TAG: name = " + xmlPullParser.getName());
+                        break;
+                    // содержимое тэга
+                    case XmlPullParser.TEXT:
+                        Log.d(LOG_TAG, "text = " + xmlPullParser.getText());
+                        break;
+
+                    default:
+                        break;
+                }
+                xmlPullParser.next();
+            }
+            Log.d(LOG_TAG, "END_DOCUMENT");
+
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private XmlPullParser prepareXpp() {
+        return getResources().getXml(R.xml.data);
     }
 
     @Override
