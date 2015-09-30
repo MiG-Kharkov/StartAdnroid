abcf6d4546e07bd4b4d8a53accd74bce6a510936
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-30 18:16:17 +0300

    Lesson 78 completed. Deprecated

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,50 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
+
+    final String TABS_TAG_1 = "Tag 1";
+    final String TABS_TAG_2 = "Tag 2";
+    TabHost.TabContentFactory TabFactory = new TabHost.TabContentFactory() {
+
+        @Override
+        public View createTabContent(String tag) {
+            if (tag == TABS_TAG_1) {
+                return getLayoutInflater().inflate(R.layout.tab, null);
+            } else if (tag == TABS_TAG_2) {
+                TextView tv = new TextView(MainActivity.this);
+                tv.setText("Это создано вручную");
+                return tv;
+            }
+            return null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec(TABS_TAG_1);
+        tabSpec.setContent(TabFactory);
+        tabSpec.setIndicator("Вкладка 1");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec(TABS_TAG_2);
+        tabSpec.setContent(TabFactory);
+        tabSpec.setIndicator("Вкладка 2");
+        tabHost.addTab(tabSpec);
     }
 
     @Override
