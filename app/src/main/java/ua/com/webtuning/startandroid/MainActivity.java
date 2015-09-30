b1eb8fd6b2b72d04c798048922607a7537848599
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-30 18:11:50 +0300

    Lesson76 + add lesson 77 with TabActivity. Deprecated

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,17 +1,15 @@
 package ua.com.webtuning.startandroid;
 
 import android.annotation.TargetApi;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TabHost;
-import android.widget.TabHost.OnTabChangeListener;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -19,46 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
-        // инициализация
-        tabHost.setup();
+        // получаем TabHost
+        TabHost tabHost = getTabHost();
+
+        // инициализация была выполнена в getTabHost
+        // метод setup вызывать не нужно
 
         TabHost.TabSpec tabSpec;
 
-        // создаем вкладку и указываем тег
         tabSpec = tabHost.newTabSpec("tag1");
-        // название вкладки
-        tabSpec.setIndicator("Tab 1");
-        // указываем id компонента из FrameLayout, он и станет содержимым
-        tabSpec.setContent(R.id.tvTab1);
-        // добавляем в корневой элемент
+        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setContent(new Intent(this, one.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag2");
-        // указываем название и картинку
-        // в нашем случае вместо картинки идет xml-файл,
-        // который определяет картинку по состоянию вкладки
-        tabSpec.setIndicator("Tab 2", getResources().getDrawable(R.drawable.tab_icon_selector, getTheme()));
-        tabSpec.setContent(R.id.tvTab2);
+        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setContent(new Intent(this, two.class));
         tabHost.addTab(tabSpec);
-
-        tabSpec = tabHost.newTabSpec("tag3");
-        // создаем View из layout-файла
-        View v = getLayoutInflater().inflate(R.layout.tab_header, null);
-        // и устанавливаем его, как заголовок
-        tabSpec.setIndicator(v);
-        tabSpec.setContent(R.id.tvTab3);
-        tabHost.addTab(tabSpec);
-
-        // вторая вкладка будет выбрана по умолчанию
-        tabHost.setCurrentTabByTag("tag2");
-
-        // обработчик переключения вкладок
-        tabHost.setOnTabChangedListener(new OnTabChangeListener() {
-            public void onTabChanged(String tabId) {
-                Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
     }
