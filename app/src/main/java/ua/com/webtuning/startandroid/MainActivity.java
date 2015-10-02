4afa88282c86d490f83dfa06d216bde1dceb572a
Igor Manzhos <manzhos.igor@gmail.com>
2015-10-02 18:04:21 +0300

    Lesson 87 completed

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,27 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyTask myTask;
+    TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+
     }
 
     @Override
@@ -33,5 +44,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onclick(View view) {
+        myTask = new MyTask();
+        myTask.execute("file_1", "file2", "file3", "file4");
+    }
+
+    private class MyTask extends AsyncTask<String, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(String... urls) {
+            try {
+                int cnt = 0;
+                for (String url : urls) {
+                    // загружаем файл
+                    downloadFile(url);
+                    // выводим промежуточные результаты
+                    publishProgress(++cnt, urls.length);
+                }
+                // разъединяемся
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private void downloadFile(String url) throws InterruptedException {
+            TimeUnit.SECONDS.sleep(2);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tvInfo.setText("Downloaded " + values[0] + " from " + values[1] + " files.");
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            tvInfo.setText("End");
+        }
     }
 }
