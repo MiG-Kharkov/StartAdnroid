85838dacfae1bea4142c72e5a917fe4cdbee451b
Igor Manzhos <manzhos.igor@gmail.com>
2015-10-03 15:27:09 +0300

    Lesson 95 completed

---
+++ app/src/main/java/ua/com/webtuning/startandroid/MyService.java
@@ -0,0 +1,89 @@
+package ua.com.webtuning.startandroid;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by mig on 03.10.15.
+ */
+public class MyService extends Service {
+
+    final String LOG_TAG = "myLogs";
+    ExecutorService es;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "MyService onCreate");
+        es = Executors.newFixedThreadPool(3);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MyService onDestroy");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "MyService onStartCommand");
+        int time = intent.getIntExtra(MainActivity.PARAM_TIME, 1);
+        PendingIntent pi = intent.getParcelableExtra(MainActivity.PARAM_PINTENT);
+
+        MyRun mr = new MyRun(time, startId, pi);
+        es.execute(mr);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private class MyRun implements Runnable {
+        int time;
+        int startId;
+        PendingIntent pi;
+
+        public MyRun(int time, int startId, PendingIntent pi) {
+            this.time = time;
+            this.startId = startId;
+            this.pi = pi;
+            Log.d(LOG_TAG, "MyRun#" + startId + " create");
+        }
+
+
+        @Override
+        public void run() {
+            Log.d(LOG_TAG, "MyRun#" + startId + " start, time = " + time);
+            try {
+                pi.send(MainActivity.STATUS_START);
+
+                TimeUnit.SECONDS.sleep(time);
+
+                Intent intent = new Intent().putExtra(MainActivity.PARAM_RESULT, time * 100);
+                pi.send(MyService.this, MainActivity.STATUS_FINISH, intent);
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            stop();
+        }
+
+        void stop() {
+            Log.d(LOG_TAG, "MyRun#" + startId + " end, stopSelfResult("
+                    + startId + ") = " + stopSelfResult(startId));
+        }
+    }
+}
