49b97b635fdaea254545fdc085067d0bd02968e2
Igor Manzhos <manzhos.igor@gmail.com>
2015-10-04 09:40:38 +0300

    Lesson 97  completed needs Service and doesn't work on Emulator

--- app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/startandroid/MainActivity.java
@@ -1,16 +1,44 @@
 package ua.com.webtuning.startandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    boolean bound = false;
+    ServiceConnection serviceConnection;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent("ua.com.webtuning.servicebindserver.MyService");
+
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.d(LOG_TAG, "MainActivity onServiceConnected");
+                bound = true;
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.d(LOG_TAG, "MainActivity onServiceDisconnected");
+                bound = false;
+            }
+        };
     }
 
     @Override
@@ -33,5 +61,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickStart(View view) {
+        startService(intent);
+    }
+
+    public void onClickStop(View view) {
+        stopService(intent);
+    }
+
+    public void onClickBind(View view) {
+        //Без флага сервси не создастся при запросе
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+    public void onClickUnBind(View view) {
+        if (!bound) return;
+        unbindService(serviceConnection);
+        bound = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        onClickUnBind(null);
     }
 }
